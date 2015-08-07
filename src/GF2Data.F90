0fe7e511885d1385100a3c3b970ccee051dfea85
George Booth <george.booth24@gmail.com>
2015-08-07 23:49:08 +0100

    More work on GF2

--- src/GF2Data.F90
+++ src/GF2Data.F90
@@ -2,5 +2,24 @@ module GF2Data
     use const
     implicit none
 
+    integer :: nMatsubara
+    real(dp), allocatable :: MatsuPoints(:)
+
+    !nImTimePoints = ScaleImTime * nMatsubara * Beta_Temp / pi
+    integer :: nImTimePoints    
+    real(dp), parameter :: ScaleImTime = 20
+    !The first point will be zero, and the last Beta_Temp
+    real(dp), allocatable :: ImTimePoints(:)
+
+    real(dp) :: Beta_Temp
+
+    complex(dp), allocatable :: SE_Matsu_GV(:,:,:)
+    complex(dp), allocatable :: SE_Tau_GV(:,:,:)
+    complex(dp), allocatable :: GLat_Matsu_GV(:,:,:)
+    complex(dp), allocatable :: GLat_Tau_GV(:,:,:)
+
+    real(dp), allocatable :: FockMat_GV(:,:)
+    real(dp), allocatable :: DensityMat_GV(:,:) !But should this be complex? 
+
 
 end module GF2Data
